00adf4295e5beb7af50a2e7c2a42d69210118c07
amirgolpaa24 <amirgolpaa24@gmail.com>
2021-11-22 14:12:56 +0330

    Update add_demo_users.py
    update add_demo_users: add function add_users()

--- add_demo_users.py
+++ add_demo_users.py
@@ -2,22 +2,22 @@
 #     go to accounting path, 
 #     source the venv, 
 #     enter: 'python3 manage.py shell', 
-#     then just import this file. Done!
+#     then import this file (add_demo_users),
+#     then just call the function: 
+#           add_users(n=1, initial_charge=100000, pwd='123') with the arguments you want. Done!
 ###########################################################
 
 from django.contrib.auth import get_user_model
 
-n = int(input(">> Enter the number of demo users:"))
-initial_charge = int(input(">> Enter the initial charge (in Tomans):"))
-pwd = input(">> Enter the password:")
 
-User = get_user_model()
+def add_users(n=1, initial_charge=100000, pwd='123'):
+    User = get_user_model()
 
-for i in range(n):
-    user = User.objects.create_user(
-        username=f"demo_user_{i + 1}",
-        password=pwd,
-        is_active=True,
-    )
-    user.wallet.add_charge(initial_charge * 10)
-    print(f"# USER {user.username} WAS CREATED SUCCESSFULLY.")
+    for i in range(n):
+        user = User.objects.create_user(
+            username=f"demo_user_{i + 1}",
+            password=pwd,
+            is_active=True,
+        )
+        user.wallet.add_charge(initial_charge * 10)
+        print(f"# USER {user.username} WAS CREATED SUCCESSFULLY.")
